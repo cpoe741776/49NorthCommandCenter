585f966a5709dae333e06cef07bb105186bc9d24
cpoe07041776 <christopher.poe@me.com>
2026-01-06 19:18:53 +0000

    Wrap secretaryRemindDue require in try/catch

--- netlify/functions/secretaryRemindDue.js
+++ netlify/functions/secretaryRemindDue.js
@@ -1,5 +1,17 @@
-const remind = require("./secretary/remind-due.js");
-
 exports.handler = async (event, context) => {
-  return remind.handler(event, context);
+  try {
+    const remind = require("./secretary/remind-due.js");
+    return await remind.handler(event, context);
+  } catch (err) {
+    console.log("secretaryRemindDue WRAPPER LOAD ERROR:", err && err.stack ? err.stack : err);
+    return {
+      statusCode: 500,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        ok: false,
+        where: "wrapper-require",
+        error: String((err && err.message) || err)
+      })
+    };
+  }
 };
